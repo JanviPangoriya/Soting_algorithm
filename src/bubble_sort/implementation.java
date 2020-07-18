4bb171f375e6be0762fb86fcf0f55c3e186b2b73
JanviPangoriya <janvi.pangoriya_cs18@gla.ac.in>
2020-07-18 04:59:46 -0700

    bubble sort implementation

---
+++ src/bubble_sort/implementation.java
@@ -0,0 +1,15 @@
+package bubble_sort;
+
+public class implementation {
+    public void bubble_sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
